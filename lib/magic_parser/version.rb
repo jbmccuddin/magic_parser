2c7596d3b626f8cd5afc8db5c32359172127909b
Jacob McCuddin <jacob.mcc.dev@gmail.com>
2025-02-23 23:11:06 -0600

    initial commit

---
+++ lib/magic_parser/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MagicParser
+  VERSION = "0.1.0"
+end
---
+++ spec/magic_parser_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MagicParser do
+  it "has a version number" do
+    expect(MagicParser::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
